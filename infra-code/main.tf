198b3b65d38ee0b4ce30a63d41297c63463169ad
Oakkharaphop Makanat <oakkharaphopmakanat@gmail.com>
2025-04-11 14:51:50 +0700

    Test terraform

---
+++ infra-code/main.tf
@@ -0,0 +1,60 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config" 
+}
+
+resource "kubernetes_deployment" "nxz_app" {
+  metadata {
+    name = "nxz-app"
+    labels = {
+      app = "nxz"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "nxz"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nxz"
+        }
+      }
+
+      spec {
+        container {
+          name  = "nxz-container"
+          image = "akkharaphopmakanat/nxz-devops-pretest:latest"
+          ports {
+            container_port = 3000
+          }
+        }
+      }
+    }
+  }
+}
+
+# Kubernetes Service
+resource "kubernetes_service" "nxz_app_service" {
+  metadata {
+    name = "nxz-service"
+  }
+
+  spec {
+    selector = {
+      app = "nxz"
+    }
+
+    port {
+      port        = 80
+      target_port = 3000
+    }
+
+    type = "LoadBalancer"
+  }
+}
